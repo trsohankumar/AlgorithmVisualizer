250905a0f75a06212d6125bdd20a847e8e4b26c6
siddharthvaddem <siddharthvaddem@yahoo.com>
2021-05-09 14:38:57 +0530

    fixed splash

--- app/src/main/java/com/example/algorithmvisualizer/MainActivity.kt
+++ app/src/main/java/com/example/algorithmvisualizer/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_AlgorithmVisualizer)
+        setTheme(R.style.Theme_Design_NoActionBar)
         setContentView(R.layout.activity_main)
     }
 }
