2cfffb0f9db3c5c5870799d6285e4b38f451829f
trsohankumar <sohankumartr@gmail.com>
2021-05-11 11:44:46 +0530

    added the white theme

--- app/src/main/java/com/example/algorithmvisualizer/MainActivity.kt
+++ app/src/main/java/com/example/algorithmvisualizer/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.algorithmvisualizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_Design_NoActionBar)
+        setTheme(R.style.Theme_NoActionBar)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate
+            .setDefaultNightMode(
+                AppCompatDelegate
+                    .MODE_NIGHT_YES);
     }
 }
